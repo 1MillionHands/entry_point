a2513f17be5bc1de5c5c79f00256f178fd0b1dc6
Itay Zohar <itaynzohar@gmail.com>
2023-11-11 13:06:08 +0200

    add .gitignore and hide sensitive data

--- get_posts.py
+++ get_posts.py
@@ -6,7 +6,7 @@ with open('config_file.json', 'r') as f:
   config_data = json.load(f)
 
 headers = {"accept": "application/json",
-           "Cookie": "CF_Authorization=eyJhbGciOiJSUzI1NiIsImtpZCI6ImE3MjQ3ODBmODdmNDY0MmZkYTlhZDM1YjFjYjJkYjBmZjVmMDAyY2JlYzM3MjQyMzY3NDBjZTI0NWJiMGQ4ZmQifQ.eyJhdWQiOlsiMjM4MTAzOWZhYTkxNTdkZTMxZjU1M2U0ZTBhYTQ1OWVlMDY2Mzc1NTgwNzRmM2U2MTA3YWEwNmQ4MmI4MTJkOSJdLCJlbWFpbCI6InYub25lbWlsbGlvbmhhbmRzMUBvY3Q3LmlvIiwiZXhwIjoxNzAxNDU5MDA0LCJpYXQiOjE2OTg4MzEwMDQsIm5iZiI6MTY5ODgzMTAwNCwiaXNzIjoiaHR0cHM6Ly90c3dpbC5jbG91ZGZsYXJlYWNjZXNzLmNvbSIsInR5cGUiOiJhcHAiLCJpZGVudGl0eV9ub25jZSI6InRiU3d2Tk5CeTkycjlVcWsiLCJzdWIiOiI2YzNiNzc0NS03ZjczLTViYjktOTczOS03MTgyM2I5ODExODIiLCJjb3VudHJ5IjoiSUwifQ.FxNh87nvyIicCW732go2RV7Q1Mxzwwq2gUXxpOQgx2S7vWpcx5dLlyl3CDvUDpSfzr9mnlNxAcBEVmPGIL64swR4KEaNS9dQUsXYwPYlnjNeuvHWMdrwNHpzcSxz-HanFHV_C5ujMqJMYScdv01Ub8KRodzovBvFrIqF4zeuZA40dwNgYb34-oNYfZZ61g52CQCiUSF0xoMA5ZZc2C63KVNl7ULS63pPAfeJgMKHgepMiXxgq__vK2ZrGjiY_kc4qhKwQVWzTzJiSHgAqdZCLNbd7lnnrBKFvqyen4gBhf7ykdQQj6gBCHeUQzV9Jz9Dn324PRsnH7XK2OkNeKDGDg"}
+           "Cookie": f"CF_Authorization={config_data['letBotsWorkToekn']}"}
 page_size = config_data.get("page_size")
 
 retry_strategy = Retry(
