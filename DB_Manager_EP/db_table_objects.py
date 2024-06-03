34f63063d00a09fc9ab8376dffc717d8d3da2baa
yanirkes <yanirkes@gmail.com>
2024-06-03 13:05:08 +0300

    adjust buildspec

--- DB_Manager_EP/db_table_objects.py
+++ DB_Manager_EP/db_table_objects.py
@@ -1,22 +1,13 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, text, ForeignKey, Boolean, Float, Enum
-from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 from enum import Enum as PythonEnum, auto
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, text, ForeignKey, TIMESTAMP, \
+    Boolean, Float, Enum, event, func
+from sqlalchemy.ext.declarative import declarative_base
 import uuid
 Base = declarative_base()
-from abc import ABC, abstractmethod
 
 
-class DbObject():
-
-    @abstractmethod
-    def transform_data(self, **kwargs):
-        pass
-
-
-
-# Define the Posts model
-class Post(Base, DbObject):
+class Post(Base):
     __tablename__ = 'Post'
 
     post_id = Column(String, primary_key=True, default=str(uuid.uuid4()))
@@ -43,73 +34,8 @@ class Post(Base, DbObject):
     date_archive = Column(DateTime, default=datetime(2000, 1, 1))
     virality_score = Column(Float, default=0)
 
-    _table_args_ = {'schema': 'omh_schema'}
+    __table_args__ = {'schema': 'omh_schema'}
 
-    def _init_(self):
-        self.platform_mapping = {
-            'linkedin': 'LINKEDIN',
-            'Linkedin': 'LINKEDIN',
-            'instagram': 'INSTAGRAM',  # Typo corrected to 'instagram'
-            'Instagram': 'INSTAGRAM',
-            'youtube': 'YOUTUBE',
-            'Youtube': 'YOUTUBE',
-            'twitter': 'TWITTER',
-            'Twitter': 'TWITTER',
-            'facebook': 'FACEBOOK',
-            'Facebook': 'FACEBOOK',
-            'tiktok': 'TIKTOK',
-            'Tiktok': 'TIKTOK'
-        }
-        self.column_names_mapping = {
-        'created_at': 'publish_date',
-        'comment_count': 'num_of_comments',
-        'like_count': 'num_of_likes',
-        'post_url': 'url',
-        'pro_israel': 'sentiment',
-        'last_scrape_time': 'timestamp',
-        'post_type': 'type',
-        'city': 'location',
-        'platform_avatar_username': 'name',
-        'is_influencer': 'is_verified',
-        'platform': 'platform_type',
-        'platform_text': 'content',
-        'platform_avatar_pic_url': 'creator_image',
-    }
-
-    def transform_data(self, posts):
-        # todo: improve algorithm
-        max_length = 255
-        for post in posts:
-            # Replace None, 'nan', or empty strings with default values or None
-            for key, value in post.items():
-                if post[key] == 'None' or str(post[key]).lower() == 'nan' or post[key] == "":
-                    post[key] = None
-                if (key == 'media_url' or key == 'creator_image') and isinstance(value, str) and len(
-                        value) > max_length:
-                    post[key] = value[:max_length]
-                elif key == 'platform_type':
-                    post[key] = self.platform_mapping.get(post[key])
-                if key == 'curr_engagement' and value:
-                    try:
-                        post[key] = int(float(value))
-                    except:
-                        # Handle the case where the value cannot be converted to float or int
-                        post[key] = 0
-
-            if post['sentiment'] == False:
-                post['sentiment'] = '-1'
-            else:
-                post['sentiment'] = '1'
-        return posts
-
-    def match_headers(self, posts):
-        # todo: improve algorithm
-        for entry in posts:
-            for old_name, new_name in self.column_names_mapping.items():
-                if old_name in entry:
-                    entry[new_name] = entry.pop(old_name)
-
-        return posts
 
 class PlatformType(PythonEnum):
     LINKEDIN = auto()
@@ -120,7 +46,7 @@ class PlatformType(PythonEnum):
     TIKTOK = auto()
 
 
-class PostHistory(Base, DbObject):
+class PostHistory(Base):
     __tablename__ = 'PostHistory'
 
     post_history_id = Column('postHistory_id', String, primary_key=True, default=str(uuid.uuid4()))
@@ -137,7 +63,7 @@ class PostHistory(Base, DbObject):
     virality_score = Column(Float, default=0)
 
 
-class Creatort(Base, DbObject):
+class Creatort(Base):
     __tablename__ = 'Creator'
 
     creator_id = Column(String, primary_key=True, default=str(uuid.uuid4()))
@@ -154,20 +80,23 @@ class Creatort(Base, DbObject):
     hashtag_list = Column(String, default='')
     engagement_history = Column(String, default='')
     last_post_date = Column(DateTime, default=datetime(2000, 1, 1))
+    platform_type = Column(String, default=str(uuid.uuid4()))
 
 
-class CreatorHistoryt(Base, DbObject):
+class CreatorHistoryt(Base):
     __tablename__ = 'CreatorHistory'
+    # now = datetime.utcnow()
 
     creator_history_id = Column('creatorHistory_id', String, primary_key=True, default=str(uuid.uuid4()))
     creator_fk = Column(String, ForeignKey('Creator.creator_id'))
     owner_post_count = Column(Integer, default=0)
     owner_follower_count = Column(Integer, default=0)
     creator_score = Column(Integer, default=0)
+    created_at = Column(TIMESTAMP, server_default=func.now())
 
 
-# class MetaDataBuckett(Base, DbObject):
-#     _tablename_ = 'meta_data_bucket'
+# class MetaDataBuckett(Base):
+#     __tablename__ = 'meta_data_bucket'
 #
 #     id = Column(String, primary_key=True, default=str(uuid.uuid4()))
 #     source_fk = Column(String, ForeignKey('meta_data_sources.id'))
@@ -175,17 +104,41 @@ class CreatorHistoryt(Base, DbObject):
 #     content = Column(String)
 #
 #
-# class MetaDataSourcest(Base, DbObject):
-#     _tablename_ = 'meta_data_sources'
+# class MetaDataSourcest(Base):
+#     __tablename__ = 'meta_data_sources'
 #
 #     id = Column(String, primary_key=True, default=str(uuid.uuid4()))
 #     name = Column(String)
 #     url = Column(String)
 
-class Volunteer(Base, DbObject):
+class Volunteer(Base):
     __tablename__ = 'meta_data_sources'
 
     volunteer_id = Column(String, primary_key=True, default=str(uuid.uuid4()))
     first_name = Column(String)
     last_name = Column(String)
     url = Column(String)
+
+# class BaseMixin(object):
+#
+#   __table_args__ = {'mysql_engine': 'InnoDB'}
+#
+#   id = Column(Integer, primary_key=True)
+#   created_at = Column('created_at', DateTime, nullable=False)
+#   updated_at = Column('updated_at', DateTime, nullable=False)
+#
+#   @staticmethod
+#   def create_time(instance):
+#      now = datetime.datetime.utcnow()
+#      instance.created_at = now
+#      instance.updated_at = now
+#
+#   @staticmethod
+#   def update_time(instance):
+#      now = datetime.datetime.utcnow()
+#      instance.updated_at = now
+#
+#   @classmethod
+#   def register(cls):
+#      event.listen(cls, 'before_insert', cls.create_time)
+#      event.listen(cls, 'before_update', cls.update_time)
